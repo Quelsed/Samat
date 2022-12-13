9b72e3b5f27ecea4fdb4c719dfe863c36a53f9fc
SRGilmullin <samatgilmullin@gmail.com>
2022-12-13 19:14:41 +0300

    add new field

--- app/src/main/java/com/example/myapplicationqq/MainActivity.java
+++ app/src/main/java/com/example/myapplicationqq/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private int a=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
